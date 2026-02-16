7d100b2f44a4201c80394222f07990ab1744c869
boxlink <luisalcca5b@gmail.com>
2026-02-15 22:51:13 -0500

    1.3

--- main/main.c
+++ main/main.c
@@ -314,6 +314,9 @@ void app_main(void)
 
     // Initialize Modbus TCP slave
     uint8_t edad = 0;
+
+    // prueba de lectura de registros de entrada antes de iniciar el slave
+    
     uint8_t eth_port_cnt = 0;
     esp_eth_handle_t *eth_handles;
     
